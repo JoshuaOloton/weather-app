ba7bce38ed75cedf7b553a46acdd46e907374370
Joshua Oloton <olotonjoshua@gmail.com>
2025-09-14 15:04:40 +0100

    feat: add responsives structure of weather dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import CurrentWeather from "@/components/CurrentWeather";
+import DailyForecast from "@/components/DailyForecast";
+import HourlyForecast from "@/components/HourlyForecast";
+import WeatherHighlight from "@/components/WeatherHighlight";
+import WeatherSearch from "@/components/WeatherSearch";
 
 export default function Home() {
   return (
-    <p>Weather app</p>
+    <main className="max-w-5xl mx-auto my-4">
+      <WeatherSearch />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+        <CurrentWeather />
+        <HourlyForecast />
+        <WeatherHighlight />
+        <DailyForecast />
+      </div>
+    </main>
   );
 }
